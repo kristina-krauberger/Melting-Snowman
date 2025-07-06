c6cb0ced4ae20889f7c58b74235e5885f682281f
Kristina Krauberger <krauberger.kristina@gmail.com>
2025-07-06 21:11:04 +0100

    Added ASCII-Art and display_game_function() and counter for mistakes

--- snowman.py
+++ snowman.py
@@ -3,10 +3,54 @@ import random
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
 
 def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
+
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    """Displays the snowman stage for the number of mistakes."""
+    # Display the snowman stage for the current number of mistakes.
+    print(STAGES[mistakes])
+    # Build a display version of the secret word.
+    display_word = ""
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    print("Word: ", display_word)
+    print("\n")
 
 
 def play_game():
@@ -20,14 +64,18 @@ def play_game():
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
-    # TODO: Build your game loop here.
-    # For now, simply prompt the user once:
+
     guessed_letters = []
+    mistakes = 0
+    display_game_state(mistakes, secret_word, guessed_letters)
+
     for i in range(len(secret_word)):
         guess = input("Guess a letter: ").strip().lower()
         guessed_letters.append(guess)
+        if guess not in secret_word:
+            mistakes += 1
+        display_game_state(mistakes, secret_word, guessed_letters)
         print("You guessed:", guess)
-        print(guessed_letters)
     if set(guessed_letters) == set(secret_word):
         print(f"You Win! The word was {secret_word}")
     else:
