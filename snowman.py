6210b118c8f904a3bda5110fa1b3d7e1e632788b
Kristina Krauberger <krauberger.kristina@gmail.com>
2025-07-07 19:10:06 +0100

    Updadet get_input function, it now only allows letters as input

--- snowman.py
+++ snowman.py
@@ -1,12 +1,18 @@
 from game_logic import play_game
 
 
-def user_input():
-    return input("Guess a letter: ").strip().lower()
+def get_input():
+    while True:
+        user_input = input("Guess a letter: ").strip().lower()
+
+        if not user_input.isalpha() or len(user_input) != 1:
+            print("Invalid input – please type **one single letter** (a-z).")
+        else:
+            return user_input
 
 
 def main():
-    play_game(user_input)    #Hier wir die Funktion user_input() übergeben nicht das Ergebnis
+    play_game(get_input)    #Hier wir die Funktion get_input() übergeben nicht das Ergebnis
 
 
 if __name__ == "__main__":
